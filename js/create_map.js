1c3a525656b0cddf8127fb42f7d0f54d857b7746
Qichun Dai <49536404+qichun-dai@users.noreply.github.com>
2024-04-08 21:56:01 +0200

    updates

---
+++ js/create_map.js
@@ -0,0 +1,8 @@
+var map = L.map('map').setView([52.3676, 4.9041], 13);
+
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+}).addTo(map);
+
